b2e50b1823c80534d732c81000803b4b196e0a5b
Akito-ce <salnikov-ds@mail.ru>
2022-09-29 02:56:20 +0300

    'quadratic'

---
+++ main2.py
@@ -0,0 +1,11 @@
+a, b, c = float(input()), float(input()), float(input())
+
+d = b ** 2 - 4 * a * c
+x1 = (-b + d ** 0.5) / (2 * a)
+x2 = (-b - d ** 0.5) / (2 * a)
+if d > 0:
+    print(f'x1 = {x1}\nx2 = {x2}')
+elif d == 0:
+    print(f'x = {x1}')
+else:
+    print('Корней нет')
