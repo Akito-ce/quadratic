124538d85099cc9dc34a4f37542956972953c8bd
Akito-ce <salnikov-ds@mail.ru>
2022-11-17 15:19:16 +0300

    'quadratic2'

--- main2.py
+++ main2.py
@@ -1,4 +1,12 @@
-a, b, c = float(input()), float(input()), float(input())
+while True:
+    a = input()
+    if a.isdigit() and a != '0':
+        a = float(a)
+        break
+    else:
+        print('Введите другое значение')
+
+b, c = float(input()), float(input())
 
 d = b ** 2 - 4 * a * c
 x1 = (-b + d ** 0.5) / (2 * a)
